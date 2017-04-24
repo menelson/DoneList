c628c3b7ea661dd46b713f8aa19688af547b6932
Mike Nelson Jr <michaeljr.nelson@gmail.com>
2017-04-24 06:49:23 -0400

    Initial Commit

---
+++ Done List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Done List
+//
+//  Created by Mike Nelson on 4/24/17.
+//  Copyright © 2017 MJR Designs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
