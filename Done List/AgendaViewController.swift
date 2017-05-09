671aceac751a075d7951eb187425e84a487da08f
Mike Nelson Jr <michaeljr.nelson@gmail.com>
2017-05-09 15:34:14 -0400

    Add fetch events for calendars

--- Done List/AgendaViewController.swift
+++ Done List/AgendaViewController.swift
@@ -12,11 +12,20 @@ import EventKit
 class AgendaViewController: UIViewController {
     
     var calendars = [EKCalendar]()
+    var agendaEvents = [EKEvent]()
+    static let ONE_DAY_INTERVAL = 24 * 3600
+    
+    @IBOutlet weak var agendaTableView: UITableView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        getPreferredCalendars()
+        getTodaysEventsFromCalendars(calendars: calendars)
+        
+        agendaTableView?.delegate = self
+        agendaTableView?.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,17 +33,52 @@ class AgendaViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getPreferredCalendars() {
+        let calendarIds = DLCalendarDefaults.init().getAvailableCalendars()
+        
+        for id in calendarIds {
+            let calendar = DLCalendarService.init().fetchCalendarById(identifier: id)
+            calendars.append(calendar)
+        }
     }
-    */
     
+    func getTodaysEventsFromCalendars(calendars: [EKCalendar]) {
+        let eventStore = EKEventStore()
+        
+        let startDate = Date()
+        let endDate = startDate.addingTimeInterval(TimeInterval(AgendaViewController.ONE_DAY_INTERVAL))
+        
+        let eventSearch = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: self.calendars)
+        
+        agendaEvents = eventStore.events(matching: eventSearch)
+    }
     
+}
+
+extension AgendaViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return agendaEvents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AgendaCell") else {
+            return UITableViewCell()
+        }
+        
+        cell.textLabel?.text = agendaEvents[indexPath.row].title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Already Scheduled"
+    }
+}
+
+extension AgendaViewController: UITableViewDelegate {
     
 }
